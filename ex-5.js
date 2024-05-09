742eb0a017bb88830d0366eb062a014c8d831284
PiPo <pipo.khomgrid@gmail.com>
2024-05-09 14:52:30 +0700

    ex1-8 javascript-primitive-value

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let productPriceTotal = 27000;
+let Discount = (100 - 25) / 100;
+
+let productpricediscount = productPriceTotal * Discount;
+console.log(productpricediscount);
